1e95c377a584fd960107935a603be425055fddfe
jeyaicloud-netizen <jeyaicloud@gmail.com>
2026-06-17 13:36:27 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ይህ ኔትሊፋይ ላይ Build ሲያደርግ የ ESLint ስህተት ቢኖርም እንኳን አልፎ እንዲሰራ ያደርገዋል
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // የ ታይፕስክሪፕት ስህተት ካጋጠመህ እሱንም አብሮ እንዲያልፈው ይህንን እንጨምራለን
     ignoreBuildErrors: true,
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
