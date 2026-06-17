203f17fc3f95af732e732522148bd225d7ce3a49
jeyaicloud-netizen <jeyaicloud@gmail.com>
2026-06-17 14:13:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // በ Build ጊዜ የ ESLint ስህተቶችን ችላ እንዲል ያደርጋል
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // አሁን የመጣብህን የ Type ስህተት (mode="wait") ሙሉ በሙሉ ችላ እንዲለው ያደርጋል
     ignoreBuildErrors: true,
-  },
+  }
 };
 
 export default nextConfig;
